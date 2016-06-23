9828c4c65c0890f7e736cac0bcbf9b7658d3c3ac
Marcus Ottosson <konstruktion@gmail.com>
2016-06-23 22:17:05 +0100

    Transitioning to Docker for tests

--- run_coverage.py
+++
@@ -1,7 +0,0 @@
-import sys
-import nose
-
-if __name__ == '__main__':
-    argv = sys.argv[:]
-    argv.extend(['-c', '.noserc'])
-    nose.main(argv=argv)
--- run_tests.py
+++
@@ -1,8 +0,0 @@
-import sys
-import nose
-
-
-if __name__ == '__main__':
-    argv = sys.argv[:]
-    argv.extend(['--include=tests', '--with-doctest', '--verbose'])
-    nose.main(argv=argv)
