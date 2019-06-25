02998539a81dd00aaf67e1be853c430e3d3fddcc
Jakub Trllo <jakub.trllo@gmail.com>
2019-06-25 14:29:05 +0200

    added TerminalDetail delegate which can show whole log detail

--- pyblish_lite/delegate.py
+++ pyblish_lite/delegate.py
@@ -455,3 +455,99 @@ class TerminalItem(QtWidgets.QStyledItemDelegate):
 
     def sizeHint(self, option, index):
         return QtCore.QSize(option.rect.width(), self.HEIGHT)
+
+
+class TerminalDetail(QtWidgets.QStyledItemDelegate):
+    """Delegate used exclusively for the Terminal"""
+
+    show_roles = {
+        model.LogLevel: 'Level',
+        model.QtCore.Qt.DisplayRole: 'Message',
+        model.LogThreadName: 'Thread',
+        model.LogName: 'Plugin',
+        model.LogFilename: 'File',
+        model.LogPath: 'Path',
+        model.LogLineNumber: 'Line',
+        model.LogMilliseconds: 'Millis'
+    }
+
+    def paint(self, painter, option, index):
+        """Paint text"""
+        html_text = ''
+        for role, title in self.show_roles.items():
+            text = index.model().data(index, role)
+            if not text:
+                continue
+
+            text = str(text).replace('\n', '<br/>')
+
+            title_tag = (
+                '<span style=\" font-size:8pt; font-weight:600;'
+                # ' background-color:#bbb; color:#333;\" >{}:</span> '
+                ' color:#fff;\" >{}:</span> '
+            ).format(title)
+
+            html_text += (
+                '<tr><td align=right><nobr>{}</nobr></td>'
+                '<td width="100%">{}</td></tr>'
+            ).format(title_tag, text)
+
+        html_text = '<table width="100%" cellspacing="3">{}</table>'.format(
+            html_text
+        )
+        document = QtGui.QTextDocument()
+        document.setHtml(html_text)
+        document.setTextWidth(option.rect.width())
+
+        painter.save()
+
+        font_color = QtGui.QColor("#aaa")
+        bg_color = QtGui.QColor("#333")
+
+        if (
+            option.state & QtWidgets.QStyle.State_MouseOver and
+            option.state & QtWidgets.QStyle.State_Selected
+        ):
+            bg_color = QtGui.QColor("#353535")
+
+        elif option.state & QtWidgets.QStyle.State_MouseOver:
+            bg_color = QtGui.QColor("#3b3b3b")
+
+        elif option.state & QtWidgets.QStyle.State_Selected:
+            bg_color = QtGui.QColor("#303030")
+
+        # Rounded corners of background rectangle
+        radius = 7.0
+        bg_path = QtGui.QPainterPath()
+        bg_path.addRoundedRect(QtCore.QRectF(option.rect), radius, radius);
+        painter.fillPath(bg_path, bg_color)
+
+        painter.translate(option.rect.x(), option.rect.y())
+
+        # Set default color for text
+        ctx = QtGui.QAbstractTextDocumentLayout.PaintContext()
+        ctx.palette.setColor(QtGui.QPalette.Text, font_color)
+        document.documentLayout().draw(painter, ctx)
+
+        painter.restore()
+
+        # Store height of detail to index data for sizeHint
+        log_size = QtCore.QSize(
+            document.idealWidth(),
+            document.size().height()
+        )
+        index.model().setData(index, log_size, model.LogSize)
+
+    def sizeHint(self, option, index):
+        size = index.data(model.LogSize)
+        if size:
+            return size
+
+        # This part is for cases when height is not set yet (not work correct)
+        text = index.model().data(index, QtCore.Qt.DisplayRole)
+
+        document = QtGui.QTextDocument()
+        document.setPlainText(text)
+        document.setTextWidth(option.rect.width())
+
+        return QtCore.QSize(document.idealWidth(), document.size().height())
