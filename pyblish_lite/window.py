b8a11c08ebe91d07cdd083581c256472c6fb1984
Chad Vernon <chadvernon@gmail.com>
2020-11-12 13:46:07 -0800

    Fix pyblish/pyblish-lite#93
    Limit scaling adjustment to Qt5 and Windows since only tested in Windows

--- pyblish_lite/window.py
+++ pyblish_lite/window.py
@@ -40,11 +40,12 @@ Todo:
 
 """
 from functools import partial
+import os
 
 from . import delegate, model, settings, util, view
 from .awesome import tags as awesome
 
-from .vendor.Qt import QtCore, QtGui, QtWidgets
+from .vendor.Qt import QtCore, QtGui, QtWidgets, Qt
 
 
 class Window(QtWidgets.QDialog):
@@ -1068,7 +1069,11 @@ class Window(QtWidgets.QDialog):
         self.info(message)
 
     def paintEvent(self, event):
-        scale = self.windowHandle().screen().logicalDotsPerInch() / 96.0
+        if Qt.__qt_version__.startswith("5") and os.name == "nt":
+            # Only tested with Windows DPI scaling
+            scale = self.windowHandle().screen().logicalDotsPerInch() / 96.0
+        else:
+            scale = 1.0
         for delegate in self._delegates:
             delegate.set_dpi_scale(scale)
         super(Window, self).paintEvent(event)
