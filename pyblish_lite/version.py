c210a0e8cb376d56c15b74068199990d6ffb1637
Jimmy-Lee Boisvert <jboisvert@rodeofx.com>
2020-11-19 09:28:58 -0500

    Increment beta version number

--- pyblish_lite/version.py
+++ pyblish_lite/version.py
@@ -2,7 +2,7 @@
 VERSION_MAJOR = 0
 VERSION_MINOR = 8
 VERSION_PATCH = 8
-VERSION_BETA = "b1"
+VERSION_BETA = "b2"
 
 version_info = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_BETA)
 version = '%i.%i.%i%s' % version_info
