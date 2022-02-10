2ab584719b534fee0704a5c774b9be0e6f66560e
Wilfried Pollan <wilfried.pollan@moqodow.com>
2022-02-10 12:47:13 +0100

    don't skip instances if is in collecting stage

--- pyblish_lite/control.py
+++ pyblish_lite/control.py
@@ -318,6 +318,7 @@ class Controller(QtCore.QObject):
                 self.was_skipped.emit(plugin)
                 continue
 
+            in_collect_stage = self.collect_state == 0
             if plugin.__instanceEnabled__:
                 instances = pyblish.logic.instances_by_plugin(
                     self.context, plugin
@@ -327,7 +328,10 @@ class Controller(QtCore.QObject):
                     continue
 
                 for instance in instances:
-                    if instance.data.get("publish") is False:
+                    if (
+                        not in_collect_stage
+                        and instance.data.get("publish") is False
+                    ):
                         pyblish.logic.log.debug(
                             "%s was inactive, skipping.." % instance
                         )
@@ -340,7 +344,7 @@ class Controller(QtCore.QObject):
                     yield (plugin, instance)
             else:
                 families = util.collect_families_from_instances(
-                    self.context, only_active=True
+                    self.context, only_active=not in_collect_stage
                 )
                 plugins = pyblish.logic.plugins_by_families(
                     [plugin], families
