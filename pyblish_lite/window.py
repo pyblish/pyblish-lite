8ee6df0311da893ee55845f786dab692653f0306
Wilfried Pollan <wilfried.pollan@moqodow.com>
2022-02-10 18:03:55 +0100

    fix: Add missing Qt import

--- pyblish_lite/window.py
+++ pyblish_lite/window.py
@@ -46,6 +46,7 @@ import os
 from . import delegate, model, settings, util, view, widgets
 from .awesome import tags as awesome
 
+import Qt
 from Qt import QtCore, QtGui, QtWidgets
 from .constants import (
     PluginStates, PluginActionStates, InstanceStates, GroupStates, Roles
