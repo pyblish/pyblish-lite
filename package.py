674f082c435650e020c8f0ecc22de2cd2df5f316
nadia.essid@zag.lan <nessid@zag.com>
2023-09-07 16:38:52 +0200

    feat: add package.py
    Add package.py for rez

---
+++ package.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+name = 'pyblish_lite'
+
+version = '0.8.12.dev'
+
+description = \
+    """
+    Lightweight graphical user interface to Pyblish
+    """
+
+authors = ['Abstract Factory and Contributors marcus@abstractfactory.com']
+
+requires = ['pyblish_base-1.4+']
+
+
+def commands():
+    env.PYTHONPATH.append('{root}')
+
+help = [['Home Page', 'https://github.com/pyblish/pyblish-lite']]
